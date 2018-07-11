eda60ce3b1ef00b44267ab0526e64d81ff5ee0c6
Vitor Silva <sirmochi45@gmail.com>
2018-07-11 17:50:47 -0300

    Added primitive login to front-end

--- concurso/script.js
+++ concurso/script.js
@@ -2,3 +2,11 @@ const dev = true
 const path = dev ? 'http://0.0.0.0:8080' : 'https://hotkoa.herokuapp.com'
 
 axios.post(path, {user:"name", pass:"secret"})
+
+const login = () => {
+    const query = {}
+    query.selector = 'login'
+    query.user = document.querySelector('#username').value
+    query.secret = document.querySelector('#password').value
+    axios.post(path, query).then(res => console.log(res))
+}

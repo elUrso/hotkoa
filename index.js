3b5949b1dc737d5af2afc8708d9b9a78dcdfc858
Vitor Silva <sirmochi45@gmail.com>
2018-07-11 17:02:33 -0300

    Inital steps to parse queries

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 8080
 
+//DB Setting up
 const mysql = require('mysql')
 const db_con =
     mysql.createConnection({
@@ -9,12 +10,14 @@ const db_con =
         database: 'instit93_concurso2018'
     })
 
+console.log("Testing DB connection")
 db_con.connect(function(err) {
             if (err) throw err;
             console.log("Connected!");
             });
 db_con.end();
 
+//Koa Setting up
 const Koa = require('koa')
 const cors = require('@koa/cors')
 const bodyParser = require('koa-bodyparser')
@@ -28,8 +31,20 @@ app.use(bodyParser())
 
 app.use(
         async ctx => {
-            ctx.body = 'Hello World'
-            console.log(ctx.request.body)
+            let query = {}
+            try {
+                query = ctx.request.body
+                if (typeof(query.selector) !== 'string') {
+                    throw new Error("Invalid request")
+                }
+            }
+            catch (e) {
+                ctx.body = '! Invalid request'
+                return ;
+            }
+            switch(query) {
+                
+            }
 })
 
 app.listen(port)

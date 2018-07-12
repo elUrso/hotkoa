bf75cb8767b5ec8979bbe87b92005b4d89ad03ff
Vitor Silva <sirmochi45@gmail.com>
2018-07-11 21:37:03 -0300

    Improved login callback

--- concurso/script.js
+++ concurso/script.js
@@ -1,12 +1,10 @@
 const dev = true
 const path = dev ? 'http://0.0.0.0:8080' : 'https://hotkoa.herokuapp.com'
 
-axios.post(path, {user:"name", pass:"secret"})
-
 const login = () => {
     const query = {}
     query.selector = 'login'
-    query.user = document.querySelector('#username').value
-    query.secret = document.querySelector('#password').value
+    query.email = document.querySelector('#username').value
+    query.hash = document.querySelector('#password').value
     axios.post(path, query).then(res => console.log(res))
 }

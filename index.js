8429badb62f3b2d955d6c7f0111764bf7ea40f45
Vitor Silva <sirmochi45@gmail.com>
2018-07-11 21:36:21 -0300

    Improve DB access

--- index.js
+++ index.js
@@ -2,20 +2,51 @@ const port = process.env.PORT || 8080
 
 //DB Setting up
 const mysql = require('mysql')
-const db_con =
-    mysql.createConnection({
-        host: '108.179.193.13',
-        user: 'instit93_con2018',
-        password: '2EE-zog-B9B-qA4',
-        database: 'instit93_concurso2018'
-    })
 
-console.log("Testing DB connection")
-db_con.connect(function(err) {
-            if (err) throw err;
-            console.log("Connected!");
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection(config);
+    }
+    query(sql) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, (err, rows) => {
+                if (err)
+                    return reject(err);
+                resolve(rows);
             });
-db_con.end();
+        });
+    }
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err)
+                    return reject(err);
+                resolve();
+            });
+        });
+    }
+}
+
+const config = {
+             host: '108.179.193.13',
+             user: 'instit93_con2018',
+             password: '2EE-zog-B9B-qA4',
+             database: 'instit93_concurso2018'
+}
+
+//DB Helper Functions
+const auth_user = async (email, hash, ctx) => {
+    const con = new Database(config)
+    let result = await con.query(`SELECT id, email, hash FROM users WHERE email LIKE "${email}"`)
+    con.close()
+    try {
+        if(result[0].hash === hash)
+            return "! Login Ok"
+    } catch(e) {
+            console.log("Invalid Query")
+    }
+    return "! Login Error"
+}
 
 //Koa Setting up
 const Koa = require('koa')
@@ -30,10 +61,11 @@ app.use(cors())
 app.use(bodyParser())
 
 app.use(
-        async ctx => {
+        async (ctx, next) => {
             let query = {}
             try {
                 query = ctx.request.body
+                console.log(query)
                 if (typeof(query.selector) !== 'string') {
                     throw new Error("Invalid request")
                 }
@@ -42,8 +74,10 @@ app.use(
                 ctx.body = '! Invalid request'
                 return ;
             }
-            switch(query) {
-                
+            switch(query.selector) {
+                case 'login':
+                    ctx.body = await auth_user(query.email, query.hash, ctx)
+                    break;
             }
 })
 

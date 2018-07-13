cbaf8889345f9b83060731255ed5d3e15b0e205b
Vitor Silva <sirmochi45@gmail.com>
2018-07-13 15:31:01 -0300

    Smooth scrool

--- concurso/script.js
+++ concurso/script.js
@@ -8,3 +8,19 @@ const login = () => {
     query.hash = document.querySelector('#password').value
     axios.post(path, query).then(res => console.log(res))
 }
+
+const call2 = (a,b,c) => (() => a(b,c));
+
+const smoothYScrool = (diff, time) => {
+    window.scrollBy(0, Math.ceil(diff/time))
+    if (time > 0)
+        setTimeout(call2(smoothYScrool, diff - Math.ceil(diff/time), time-1), 1)
+}
+
+const smooth = (id) => {
+    let origin = window.pageYOffset
+    let destination = document.querySelector(id).offsetTop
+    let diff = destination - origin
+    let time = 80
+    smoothYScrool(diff, time)
+}

069278df4794a13b30cd89c3753e0a94fe41bfe9
Vitor Silva <sirmochi45@gmail.com>
2018-07-17 16:03:02 -0300

    Inital file validation

--- php/inscricao.php
+++ php/inscricao.php
@@ -1,5 +1,28 @@
 <?php
 
+# File upload configuration
+
+$pdfsent = false;
+$validpdf = false;
+$errpdf = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" and isset($_FILES["pdfdocument"])) {
+    $file = $_FILES["pdfdocument"];
+    $pdfsent = true;
+    $pdf_path = "pdf/";
+    $pdf_name = "test.pdf";
+    $extension = pathinfo(strtolower(basename($file['name'])), PATHINFO_EXTENSION);
+    $validpdfname = true;
+    if (file_exists($pdf_path . $pdf_name)) $validpdfname = false;
+    $validpdfsize = false;
+    if ($file["size"] < 5000000) $validpdfsize = true;
+    $validpdfextension = false;
+    if ($extension == "pdf") $validpdfextension = true;
+    if ($validpdfname and $validpdfextension and $validpdfsize) $validpdf = true;
+}
+
+# Validation Variables
+
 $status = "GET";
 
 $firstname = "";
@@ -26,13 +49,53 @@ $street = "";
 $validstreet = false;
 $errstreet = "";
 
+$street2 = "";
+
+$number = "";
+
 $area = "";
 $validarea = false;
 $errarea = "";
 
+
 $city = "";
 $validcity = false;
 $errcity = "";
+
+$state = "AC";
+$validstate = true;
+
+$email = "";
+$validemail= false;
+$erremail = "";
+
+$emailverify = "";
+$validemailverify = false;
+$erremailverify = "";
+
+$ddd1 = "";
+$validddd1 = false;
+$errddd1 = "";
+
+$phone1 = "";
+$validphone1= false;
+$errphone1 = "";
+
+$ddd2 = "";
+$validddd2 = false;
+$errddd2 = "";
+
+$phone2 = "";
+$validphone2= false;
+$errphone2 = "";
+
+$role = "";
+$validrole = false;
+$errrole = "";
+
+$degree = "";
+$validdegree = false;
+$errdegree = "";
 
 function validate_input($data) {
     $data = trim($data);
@@ -112,6 +175,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $errstreet = "Logradouro em branco.";
     }
+    $number = validate_input($_POST["number"]);
+    $street2 = validate_input($_POST["street2"]);
     $area = validate_input($_POST["area"]);
     if(is_string($area) and $area != "") {
         $validarea = true;
@@ -123,6 +188,143 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $validcity = true;
     } else {
         $errcity = "Cidade em branco.";
+    }
+    $statecode = validate_input($_POST["state"]);
+    if(is_string($statecode) and $statecode != "") {
+        switch ($statecode) {
+            case "AC" :
+                $state = "AC";
+                break;
+            case "AL" :
+                $state = "AL";
+                break;
+            case "AP" :
+                $state = "AP";
+                break;
+            case "AM" :
+                $state = "AM";
+                break;
+            case "BA" :
+                $state = "BA";
+                break;
+            case "CE" :
+                $state = "CE";
+                break;
+            case "DF" :
+                $state = "DF";
+                break;
+            case "ES" :
+                $state = "ES";
+                break;
+            case "GO" :
+                $state = "GO";
+                break;
+            case "MA" :
+                $state = "MA";
+                break;
+            case "MT" :
+                $state = "MT";
+                break;
+            case "MS" :
+                $state = "MS";
+                break;
+            case "MG" :
+                $state = "MG";
+                break;
+            case "PA" :
+                $state = "PA";
+                break;
+            case "PB" :
+                $state = "PB";
+                break;
+            case "PR" :
+                $state = "PR";
+                break;
+            case "PE" :
+                $state = "PE";
+                break;
+            case "PI" :
+                $state = "PI";
+                break;
+            case "RJ" :
+                $state = "RJ";
+                break;
+            case "RN" :
+                $state = "RN";
+                break;
+            case "RS" :
+                $state = "RS";
+                break;
+            case "RO" :
+                $state = "RO";
+                break;
+            case "RR" :
+                $state = "RR";
+                break;
+            case "SC" :
+                $state = "SC";
+                break;
+            case "SP" :
+                $state = "SP";
+                break;
+            case "SE" :
+                $state = "SE";
+                break;
+            case "TO" :
+                $state = "TO";
+                break;
+            default:
+                $state = "AC";
+                break;
+        }
+    }
+    $email = validate_input($_POST["email"]);
+    if(is_string($email) and $email != "" and filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $validemail = true;
+    } else {
+        $erremail = "E-mail inválido.";
+    }
+    $emailverify = validate_input($_POST["emailverify"]);
+    if(is_string($emailverify) and $emailverify == $email) {
+        $validemailverify = true;
+    } else {
+        $erremailverify = "E-mails não coincidem.";
+    }
+    $ddd1 = validate_input($_POST["ddd1"]);
+    if(is_string($ddd1) and $ddd1 != "" and !(preg_match("/[^0123456789]/", $ddd1)) and strlen($ddd1) == 2) {
+        $validddd1 = true;
+    } else {
+        $errddd1 = "DDD inválido.";
+    }
+    $phone1 = validate_input($_POST["phone1"]);
+    if(is_string($phone1) and $phone1 != "" and !(preg_match("/[^0123456789]/", $phone1)) and (strlen($phone1) == 8 or strlen($phone1) == 9)) {
+        $validphone1 = true;
+    } else {
+        $errphone1 = "Telefone inválido.";
+    }
+    $ddd2 = validate_input($_POST["ddd2"]);
+    if((is_string($ddd2) and $ddd2 != "" and !(preg_match("/[^0123456789]/", $ddd2)) and strlen($ddd2) == 2) or strlen($ddd2) == 0) {
+        $validddd2 = true;
+    } else {
+        $errddd2 = "DDD inválido.";
+    }
+    $phone2 = validate_input($_POST["phone2"]);
+    if((is_string($phone2) and $phone2 != "" and !(preg_match("/[^0123456789]/", $phone2)) and (strlen($phone2) == 8 or strlen($phone2) == 9)) or strlen($phone2) == 0) {
+        $validphone2 = true;
+    } else {
+        $errphone2 = "Telefone inválido.";
+    }
+    $role = validate_input($_POST["role"]);
+    if(is_string($role) and $role != "") {
+        $validrole = true;
+    } else {
+        $errrole = "Cargo nome em branco.";
+    }
+    $degree = validate_input($_POST["degree"]);
+    if(is_string($degree) and $degree != "") {
+        $validdegree = true;
+    } else {
+        $errdegree = "Formação em branco.";
     }
 }
 
@@ -164,10 +366,10 @@ echo $status;
     <input type="text" name="street" value="<?php echo $street?>"/>
     <p class="error">* <?php echo $errstreet; ?></p>
     <p>Número:</p>
-    <input type="text" name="number"/>
+    <input type="text" name="number" value="<?php echo $number?>"/>
     <p class="error"></p>
     <p>Complemento:</p>
-    <input type="text" name="street2"/>
+    <input type="text" name="street2" value="<?php echo $street2?>"/>
     <p class="error"></p>
     <p>Bairro:</p>
     <input type="text" name="area" value="<?php echo $area?>"/>
@@ -177,58 +379,59 @@ echo $status;
     <p class="error">* <?php echo $errcity; ?></p>
     <p>Estado:</p>
     <select name="state">
-        <option value="AC">AC</option>
-        <option value="AL">AL</option>
-        <option value="AP">AP</option>
-        <option value="AM">AM</option>
-        <option value="BA">BA</option>
-        <option value="CE">CE</option>
-        <option value="DF">DF</option>
-        <option value="ES">ES</option>
-        <option value="GO">GO</option>
-        <option value="MA">MA</option>
-        <option value="MT">MT</option>
-        <option value="MS">MS</option>
-        <option value="MG">MG</option>
-        <option value="PA">PA</option>
-        <option value="PB">PB</option>
-        <option value="PR">PR</option>
-        <option value="PE">PE</option>
-        <option value="PI">PI</option>
-        <option value="RJ">RJ</option>
-        <option value="RN">RN</option>
-        <option value="RS">RS</option>
-        <option value="RO">RO</option>
-        <option value="RR">RR</option>
-        <option value="SC">SC</option>
-        <option value="SP">SP</option>
-        <option value="SE">SE</option>
-        <option value="TO">TO</option>
+        <option value="AC" <?php if($state == "AC") echo "selected"; ?>>AC</option>
+        <option value="AL" <?php if($state == "AL") echo "selected"; ?>>AL</option>
+        <option value="AP" <?php if($state == "AP") echo "selected"; ?>>AP</option>
+        <option value="AM" <?php if($state == "AM") echo "selected"; ?>>AM</option>
+        <option value="BA" <?php if($state == "BA") echo "selected"; ?>>BA</option>
+        <option value="CE" <?php if($state == "CE") echo "selected"; ?>>CE</option>
+        <option value="DF" <?php if($state == "DF") echo "selected"; ?>>DF</option>
+        <option value="ES" <?php if($state == "ES") echo "selected"; ?>>ES</option>
+        <option value="GO" <?php if($state == "GO") echo "selected"; ?>>GO</option>
+        <option value="MA" <?php if($state == "MA") echo "selected"; ?>>MA</option>
+        <option value="MT" <?php if($state == "MT") echo "selected"; ?>>MT</option>
+        <option value="MS" <?php if($state == "MS") echo "selected"; ?>>MS</option>
+        <option value="MG" <?php if($state == "MG") echo "selected"; ?>>MG</option>
+        <option value="PA" <?php if($state == "PA") echo "selected"; ?>>PA</option>
+        <option value="PB" <?php if($state == "PB") echo "selected"; ?>>PB</option>
+        <option value="PR" <?php if($state == "PR") echo "selected"; ?>>PR</option>
+        <option value="PE" <?php if($state == "PE") echo "selected"; ?>>PE</option>
+        <option value="PI" <?php if($state == "PI") echo "selected"; ?>>PI</option>
+        <option value="RJ" <?php if($state == "RJ") echo "selected"; ?>>RJ</option>
+        <option value="RN" <?php if($state == "RN") echo "selected"; ?>>RN</option>
+        <option value="RS" <?php if($state == "RS") echo "selected"; ?>>RS</option>
+        <option value="RO" <?php if($state == "RO") echo "selected"; ?>>RO</option>
+        <option value="RR" <?php if($state == "RR") echo "selected"; ?>>RR</option>
+        <option value="SC" <?php if($state == "SC") echo "selected"; ?>>SC</option>
+        <option value="SP" <?php if($state == "SP") echo "selected"; ?>>SP</option>
+        <option value="SE" <?php if($state == "SE") echo "selected"; ?>>SE</option>
+        <option value="TO" <?php if($state == "TO") echo "selected"; ?>>TO</option>
     </select>
-    <p class="error"></p>
+    <p class="error">*</p>
     <h3>Contato:</h3>
     <p>E-mail:</p>
-    <input type="text" name="email"/>
-    <p class="error"></p>
+    <input type="text" name="email" value="<?php echo $email;?>"/>
+    <p class="error">* <?php echo $erremail;?></p>
     <p>Repetir e-mail:</p>
-    <input type="text" name="emailverify"/>
-    <p class="error"></p>
-    <p>Telefone principal:</p>
-    (0<input type="text" class="ddd" name="ddd1"/>)
-    <input type="text" name="phone1"/>
-    <p class="error"></p>
-    <p>Telefone secundário:</p>
-    (0<input type="text" class="ddd" name="ddd2"/>)
-    <input type="text" name="phone2"/>
-    <p class="error"></p>
+    <input type="text" name="emailverify"  value="<?php echo $emailverify;?>"/>
+    <p class="error">* <?php echo $erremailverify;?></p>
+    <p>Telefone principal (Apenas números):</p>
+    (0<input type="text" class="ddd" name="ddd1" value="<?php echo $ddd1;?>"/>)
+    <input type="text" name="phone1"  value="<?php echo $phone1;?>"/>
+    <p class="error">* <?php echo $errddd1 . $errphone1;?></p>
+    <p>Telefone secundário  (Apenas números):</p>
+    (0<input type="text" class="ddd" name="ddd2" value="<?php echo $ddd2;?>"/>)
+    <input type="text" name="phone2" value="<?php echo $phone2;?>"/>
+    <p class="error"><?php echo $errddd2 . $errphone2;?></p>
     <p>Cargo:</p>
-    <input type="text" name="role"/>
-    <p class="error"></p>
+    <input type="text" name="role" value="<?php echo $role;?>"/>
+    <p class="error">* <?php echo $errrole;?></p>
     <p>Formação Acadêmica:</p>
-    <input type="text" name="degree"/>
-    <p class="error"></p>
+    <input type="text" name="degree" value="<?php echo $degree;?>"/>
+    <p class="error">* <?php echo $errdegree;?></p>
     <h3>Comprovante de Docência</h3>
-    <p>Documento no formato PDF com tamanho máximo de 10MB</p>
+    <p>Documento no formato PDF com tamanho máximo de 5MB</p>
+    <input type="hidden" name="MAX_FILE_SIZE" value="5000000" />
     <input type="file" name="pdfdocument"/>
     <p class="error"></p>
     <h2>Dados da Instituição</h2>
@@ -288,7 +491,7 @@ echo $status;
 const wordcount = () => {
     const text = document.querySelector("#summary");
     const count = document.querySelector("#count");
-    count.innerText = text.innerHTML.replace(/[^A-Z0-9ãõçâêôáéíóúàü]/ig, " ").trim().split(/\s+/).length;
+    count.innerText = text.value.replace(/[^A-Z0-9ãõçâêôáéíóúàü\-]/ig, " ").trim().split(/\s+/).length;
 };
 const addmember = (Name, Role, CPF) => {
     Name = (Name)?String(Name):String();

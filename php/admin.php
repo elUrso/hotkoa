fa0d6385709397ecc32d2857f2920f76c408a7ec
Vitor Silva <sirmochi45@gmail.com>
2018-07-25 16:59:32 -0300

    Add detailed view

--- php/admin.php
+++ php/admin.php
@@ -5,6 +5,7 @@ include "config.php";
 # Login variables
 
 $login = false;
+$detail = false;
 
 function validate_input($data) {
     $data = trim($data);
@@ -35,6 +36,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             break;
     }
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" and isset($_GET["id"]) and !(preg_match("/[^0123456789]/", $_GET["id"]))) {
+    #view an specific project
+    $detail = true;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -51,21 +57,72 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <?php
 if($login) {
     readfile("panel.htm");
-    $Database_connection = mysqli_connect("localhost", $Database_Username, $Database_Password, $Database_Name);
-    if($Database_connection === false) {
-        echo "<p>Não foi possível realizar a consulta.</p>";
-    } else {
-        mysqli_set_charset($Database_connection,"utf8");
-        $subscriptionquery = mysqli_query($Database_connection, 'SELECT id, title, cpf, category, status FROM subscriptions2018;');
-        if(mysqli_num_rows($subscriptionquery)>0) {
-            echo "<script>";
-            while($row = mysqli_fetch_assoc($subscriptionquery)) {
-                echo "addProject('" . ($row["id"] + 100). "','" . $row["title"] . "','" . $row["cpf"] . "','" . $row["category"] . "','" . $row["status"] . "');";
-            }
-            echo "</script>";
+    if($detail) {
+        $Database_connection = mysqli_connect("localhost", $Database_Username, $Database_Password, $Database_Name);
+        if($Database_connection === false) {
+            echo "<p>Não foi possível realizar a consulta.</p>";
         } else {
-            echo "<p>Não foi econtrado nenhum projeto cadatrado.</p>";
+            mysqli_set_charset($Database_connection,"utf8");
+            $subscriptionquery = mysqli_query($Database_connection, 'SELECT id, firstname , secondname, rg, cpf, cep, street, street2, 
+            number, area, city, state, email, ddd1, phone1, ddd2, phone2, role, degree, institution, 
+            unity, category, theme, title, date, video, summary, members, partners, pdf, status FROM subscriptions2018 WHERE id LIKE "' . ($_GET["id"]-100) . '";');
+            if(mysqli_num_rows($subscriptionquery)>0) {
+                echo "<script>";
+                while($row = mysqli_fetch_assoc($subscriptionquery)) {
+                    echo "addDetailed(` " . ($row["id"] + 100). "`,`" . 
+                    validate_input($row["firstname"]) . "`,`" . 
+                    validate_input($row["secondname"]) . "`,`" . 
+                    validate_input($row["rg"]) . "`,`" .
+                    validate_input($row["cpf"]) . "`,`" . 
+                    validate_input($row["cep"]) . "`,`" . 
+                    validate_input($row["street"]) . "`,`" . 
+                    validate_input($row["street2"]) . "`,`" . 
+                    validate_input($row["number"]) . "`,`" . 
+                    validate_input($row["area"]) . "`,`" . 
+                    validate_input($row["city"]) . "`,`" . 
+                    validate_input($row["state"]) . "`,`" . 
+                    validate_input($row["email"]) . "`,`" . 
+                    validate_input($row["ddd1"]) . "`,`" . 
+                    validate_input($row["phone1"]) . "`,`" . 
+                    validate_input($row["ddd2"]) . "`,`" . 
+                    validate_input($row["phone2"]) . "`,`" . 
+                    validate_input($row["role"]) . "`,`" . 
+                    validate_input($row["degree"]) . "`,`" . 
+                    validate_input($row["institution"]) . "`,`" . 
+                    validate_input($row["unity"]) . "`,`" . 
+                    validate_input($row["category"]) . "`,`" . 
+                    validate_input($row["theme"]) . "`,`" . 
+                    validate_input($row["title"]) . "`,`" . 
+                    validate_input($row["date"]) . "`,`" . 
+                    validate_input($row["video"]) . "`,`" . 
+                    validate_input($row["summary"]) . "`,`" . 
+                    validate_input($row["members"]) . "`,`" . 
+                    validate_input($row["partners"]) . "`,`" . 
+                    validate_input($row["pdf"]) . "`,`" . 
+                    validate_input($row["status"]) . "`)";
+                }
+                echo "</script>";
+            } else {
+                echo "<p>Não foi econtrar o projeto especificado.</p>";
+            }
         }
+    } else {
+        $Database_connection = mysqli_connect("localhost", $Database_Username, $Database_Password, $Database_Name);
+        if($Database_connection === false) {
+            echo "<p>Não foi possível realizar a consulta.</p>";
+        } else {
+            mysqli_set_charset($Database_connection,"utf8");
+            $subscriptionquery = mysqli_query($Database_connection, 'SELECT id, title, cpf, category, status FROM subscriptions2018;');
+            if(mysqli_num_rows($subscriptionquery)>0) {
+                echo "<script>";
+                while($row = mysqli_fetch_assoc($subscriptionquery)) {
+                    echo "addProject('" . ($row["id"] + 100). "','" . $row["title"] . "','" . $row["cpf"] . "','" . $row["category"] . "','" . $row["status"] . "');";
+                }
+                echo "</script>";
+            } else {
+                echo "<p>Não foi econtrado nenhum projeto cadatrado.</p>";
+            }
+        }    
     }
 } else {
     readfile("login.htm");

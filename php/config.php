4a4e73aae22215bb68cae087f712d102729e248c
Vitor Silva <sirmochi45@gmail.com>
2018-07-26 10:27:25 -0300

    Utilize session and password from DB

--- php/config.php
+++ php/config.php
@@ -1,10 +1,28 @@
 <?php
 
+# CREATE TABLE admin2018 (username VARCHAR(255) PRIMARY KEY, password VARCHAR(255) NOT NULL, session VARCHAR(255));
+
 $Database_Name = 'test';
 $Database_Username = 'root';
 $Database_Password = 'root';
 
-$password = "b7e94be513e96e8c45cd23d162275e5a12ebde9100a425c4ebcdd7fa4dcd897c";
-$session = "b7e94be513e96e8c45cd23d162275e5a12ebde9100a425c4ebcdd7fa4dcd897c";
+$password = "";
+$session = "";
+
+$Database_connection = mysqli_connect("localhost", $Database_Username, $Database_Password, $Database_Name);
+if($Database_connection === false) {
+    $status = "Error connecting to DB";
+} else {
+    mysqli_set_charset($Database_connection,"utf8");
+    $selectquery = mysqli_query($Database_connection, 'SELECT password, session FROM admin2018 WHERE username LIKE "admin";');
+    if(mysqli_num_rows($selectquery)>0) {
+        while($row = mysqli_fetch_assoc($selectquery)) {
+            $password = $row["password"];
+            $session = $row["session"];
+        }
+    } else {
+        $status = "Error in query";
+    }
+}
 
 ?>
